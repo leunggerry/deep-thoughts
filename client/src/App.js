daaf37adb7742ae45272d5b0baea8c494ea744fa
gigi.upNorth <leung.gerry@gmail.com>
2022-10-17 13:55:04 -0400

    [Close #4] GQL queries and navigation
    Descirption:
    Adding thoughts and friends list GQL queries
    getting single thought and reaction count

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,18 @@ import React from "react";
  * createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
  */
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+//import components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 // establish new link to graphQL server
 const httpLink = createHttpLink({
@@ -31,13 +39,28 @@ function App() {
   return (
     // pass the client variable as the value for ht eclient prop
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      {/* Router component makes all child components on the page aware of the client-side routing */}
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            {/* Routes hold several Route components that signify this part of th app
+                where the content  will change according to the url route */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route path="/thought/:id" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
